858cf5730d6634e59d29eb1cc3d4ae8251b36386
Pierre Bourgeois <dragonralph@gmail.com>
2016-01-27 10:05:53 +0100

    Local ips

--- TP_1/src/LocalIPS.java
+++ TP_1/src/LocalIPS.java
@@ -1,5 +1,10 @@
 
+import java.net.InetAddress;
+import java.net.InterfaceAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -13,13 +18,37 @@ public class LocalIPS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         try {
-            System.out.println(java.net.Inet4Address.getLocalHost().toString());
-        } catch (UnknownHostException ex) {
-            System.out.println("truuc");
+            //      System.out.println(java.net.Inet4Address.getLoopbackAddress());
+            //     System.out.println(java.net.Inet4Address.getLocalHost().toString());
+            Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+            
+            /**
+             * Interfaces
+             */
+            while (networkInterfaces.hasMoreElements()) {
+                NetworkInterface nextElement = networkInterfaces.nextElement();
+                
+                System.out.println("+++ " + nextElement.getDisplayName() + " +++");
+                
+                /**
+                 * Addresses
+                 */
+                Enumeration<InetAddress> address = nextElement.getInetAddresses();
+                while(address.hasMoreElements()){
+                    InetAddress addr = address.nextElement();
+                    System.out.println("\t"+addr.toString() + "\t"+ addr.getHostName());
+                }
+                System.out.println();
+            }
+            
+            
+            
+        } catch (SocketException ex) {
+            Logger.getLogger(LocalIPS.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
-    
+
 }
